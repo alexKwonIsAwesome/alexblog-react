880cebde43760c72584002e9fa16f453368039a2
alexKwonIsAwesome <khwalex@gmail.com>
2017-01-17 16:16:06 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,13 @@
+## Alexblog
+
+# Simple blog app made with react, redux.
+
+Major dependencies:
+- react
+- react-router
+- redux
+- react-redux
+- redux-saga
+- firebase
+- reactfire
+- firebase-tools
---
+++ src/firebase/index.js
@@ -0,0 +1,7 @@
+export const config = {
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  databaseURL: process.env.DATABASE_URL,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID
+};
