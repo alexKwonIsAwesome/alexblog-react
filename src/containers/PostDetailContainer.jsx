d6ad0894eaf95709915aceeeb8f81620c2145617
alexKwonIsAwesome <khwalex@gmail.com>
2017-01-23 14:31:55 +0900

    update code: parseInt string => integer

--- src/containers/PostDetailContainer.jsx
+++ src/containers/PostDetailContainer.jsx
@@ -2,10 +2,10 @@ import { connect } from 'react-redux';
 import PostDetail from '../components/PostDetail';
 
 const mapStateToProps = (state, ownProps) => {
-  const postId = ownProps.id;
+  const postId = parseInt(ownProps.id, 10);
   const posts = state.posts;
 
-  let post = posts.find(post => post.id == postId);
+  let post = posts.find(post => post.id === postId);
   console.log(post);
   return {
     post: post
