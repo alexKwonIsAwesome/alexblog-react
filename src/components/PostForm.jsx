a00ed239bd235e22456717225a284f6590892319
alexKwonIsAwesome <khwalex@gmail.com>
2017-01-24 16:23:57 +0900

    update PostForm: input field and state management
    Form's input field is added and each field's content is managed with the
    component state. initial state is passed in from redux store as props
    and when user types contents, input's onChange triggers updatePostState
    function which updates state.

--- src/components/PostForm.jsx
+++ src/components/PostForm.jsx
@@ -1,11 +1,36 @@
 import React from 'react';
 
 class PostForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      postTitle: this.props.post.title,
+      postContent: this.props.post.content
+    }
+  }
+
+  updatePostState() {
+    const postTitle = this.refs.postTitle.value;
+    const postContent = this.refs.postContent.value;
+    this.setState({
+      postTitle: postTitle,
+      postContent: postContent
+    });
+  }
+
   render() {
     return (
       <div>
-        <h1>HAHAHAHALOVELY</h1>
-        <p>{this.props.post.id}</p>
+        <form>
+          <div className="form-group">
+            <label>Title</label>
+            <input type="text" ref="postTitle" onChange={() => this.updatePostState()} className="form-control" id="InputTitle" value={this.state.postTitle} aria-describedby="postTitle" placeholder="Post Title"/>
+          </div>
+          <div className="form-group">
+            <label>Content</label>
+            <textarea className="form-control" ref="postContent" onChange={() => this.updatePostState()} id="AreaContent" value={this.state.postContent} placeholder="Post Content" />
+          </div>
+        </form>
         <button onClick={() => this.props.toggleEdit()}>get back!</button>
       </div>
     );
@@ -13,7 +38,7 @@ class PostForm extends React.Component {
 }
 
 PostForm.propTypes = {
-  post: React.PropTypes.object.isRequired
+  post: React.PropTypes.object
 };
 
 export default PostForm;
